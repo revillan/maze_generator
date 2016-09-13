fffebcfb6bd894836962388a0dcc4e50bc1c1e15
Reilly Villanueva <reillyvillanueva@gmail.com>
2016-09-13 11:23:43 -0700

    min-heap class written

--- bundle.js
+++ bundle.js
@@ -60,8 +60,96 @@
 /* 1 */
 /***/ function(module, exports) {
 
-	// min-heap logic for edge cells
 	"use strict";
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	// min-heap logic for edge cells
+	
+	var MinHeap = function () {
+	  function MinHeap() {
+	    _classCallCheck(this, MinHeap);
+	
+	    this.array = [null];
+	    this.heapsize = 0;
+	    this.heap = {};
+	  }
+	
+	  _createClass(MinHeap, [{
+	    key: "extractMin",
+	    value: function extractMin() {
+	      if (this.heapsize < 1) {
+	        throw "heap is empty";
+	      }
+	      var min = this.array[1];
+	      this.array[1] = this.array[this.heapsize];
+	      this.heapsize--;
+	      this.minHeapify(1);
+	      return min;
+	    }
+	  }, {
+	    key: "heapInsert",
+	    value: function heapInsert(key) {
+	      this.heapsize++;
+	      this.array[this.heapsize] = 25; //basically infinitely
+	      this.heapDecreaseKey(this.heapsize, key);
+	    }
+	  }, {
+	    key: "heapDecreaseKey",
+	    value: function heapDecreaseKey(i, key) {
+	      if (key > this.array[i]) {
+	        throw "new key is bigger than old key";
+	      }
+	      this.array[i] = key;
+	      while (i > 1 && this.array[this.parent(i)] > this.array[i]) {
+	        var temp = this.array[i];
+	        this.array[i] = this.array[this.parent(i)];
+	        this.array[this.parent(i)] = temp;
+	        i = this.parent(i);
+	      }
+	    }
+	  }, {
+	    key: "minHeapify",
+	    value: function minHeapify(i) {
+	      var left = this.left(i);
+	      var right = this.right(i);
+	      var smallest = void 0;
+	      if (left <= this.heapsize && this.array[left] < this.array[i]) {
+	        smallest = left;
+	      } else {
+	        smallest = i;
+	      }
+	      if (right <= this.heapsize && this.array[right] < this.array[smallest]) {
+	        smallest = right;
+	      }
+	      if (smallest !== i) {
+	        var temp = this.array[i];
+	        this.array[i] = this.array[smallest];
+	        this.array[smallest] = temp;
+	        this.minHeapify(smallest);
+	      }
+	    }
+	  }, {
+	    key: "left",
+	    value: function left(i) {
+	      return 2 * i;
+	    }
+	  }, {
+	    key: "right",
+	    value: function right(i) {
+	      return 2 * i + 1;
+	    }
+	  }, {
+	    key: "parent",
+	    value: function parent(i) {
+	      return Math.floor(i / 2);
+	    }
+	  }]);
+
+	  return MinHeap;
+	}();
 
 /***/ },
 /* 2 */
--- queue.js
+++ queue.js
@@ -1,1 +1,72 @@
 // min-heap logic for edge cells
+
+class MinHeap {
+  constructor() {
+    this.array = [null];
+    this.heapsize = 0;
+    this.heap = {};
+  }
+
+  extractMin() {
+    if (this.heapsize < 1 ) {
+      throw "heap is empty";
+    }
+    const min = this.array[1];
+    this.array[1] = this.array[this.heapsize];
+    this.heapsize--;
+    this.minHeapify(1);
+    return min;
+  }
+
+  heapInsert(key) {
+    this.heapsize++;
+    this.array[this.heapsize] = 25; //basically infinitely
+    this.heapDecreaseKey(this.heapsize, key);
+  }
+
+  heapDecreaseKey(i, key) {
+    if (key > this.array[i]) {
+      throw "new key is bigger than old key";
+    }
+    this.array[i] = key;
+    while (i > 1 && this.array[this.parent(i)] > this.array[i]) {
+      let temp = this.array[i];
+      this.array[i] = this.array[this.parent(i)];
+      this.array[this.parent(i)] = temp;
+      i = this.parent(i);
+    }
+
+  }
+
+  minHeapify(i) {
+    const left = this.left(i);
+    const right = this.right(i);
+    let smallest;
+    if (left <= this.heapsize && this.array[left] < this.array[i]) {
+      smallest = left;
+    } else {
+      smallest = i;
+    }
+    if (right <= this.heapsize && this.array[right] < this.array[smallest]) {
+      smallest = right;
+    }
+    if (smallest !== i) {
+      let temp = this.array[i];
+      this.array[i] = this.array[smallest];
+      this.array[smallest] = temp;
+      this.minHeapify(smallest);
+    }
+  }
+
+  left(i) {
+    return (2*i);
+  }
+
+  right(i) {
+    return (2*i + 1);
+  }
+
+  parent(i) {
+    return Math.floor(i/2);
+  }
+}
